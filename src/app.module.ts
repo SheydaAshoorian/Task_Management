dcfb24a668322f8d0dc2e8c191527f908839c7c8
sheydaashoorian <sheydaashoorian@gmail.com>
2026-02-20 21:52:45 +0330

    add user maddule and user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true, // باعث می‌شود در تمام ماژول‌ها به env دسترسی داشته باشی
     }),
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,56 @@
+import { Entity , Column, PrimaryGeneratedColumn, CreateDateColumn , UpdateDateColumn, OneToMany} from 'typeorm';
+import { Task } from '../../tasks/entities/task.entity';
+
+export enum  UserRole{
+
+    Admin = 'admin',
+    User = 'user',
+    Manager = 'manager',
+}
+
+
+@Entity('User')
+export class User{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    first_name: string;
+
+    @Column()
+    last_name: string;
+
+    @Column({ unique: true }) 
+    email: string;
+
+    @Column(
+        { type : 'enum',
+          enum : UserRole,
+          default : UserRole.User , 
+        }
+    )
+    role: UserRole ;
+
+    @Column({ select: false})
+    password: string;
+
+
+    @Column({ default: true})
+    isActive: boolean;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @OneToMany(() => Task, (task) => task.assignedTo)
+    assignedTasks: Task[]; // تسک‌هایی که به این کاربر محول شده
+
+    @OneToMany(() => Task, (task) => task.createdBy)
+    createdTasks: Task[]; // تسک‌هایی که این کاربر ساخته است
+
+
+
+}
