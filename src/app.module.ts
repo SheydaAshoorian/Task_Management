bba20164a730148979cf96641b9ab4eddb6570aa
sheydaashoorian <sheydaashoorian@gmail.com>
2026-02-20 21:10:28 +0330

    add main setting for get port from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // باعث می‌شود در تمام ماژول‌ها به env دسترسی داشته باشی
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService} from '@nestjs/config';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enablrCors(); // برای اینکه فرانت‌اِند بتونه بهش وصل شه
+  app.setGlobalPefix('api/v1');  // ورژن‌بندی API
+
+  const configService = ConfigService.get(ConfigService);
+  const port = configService.get(PORT) || 3001 ;
+
+  await app.listen(PORT);
+
+  console.log(`Server is running on: http://localhost:${port}/api/v1`);
+
 }
+
 bootstrap();
