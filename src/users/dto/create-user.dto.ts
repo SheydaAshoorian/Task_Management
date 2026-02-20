723d0d8db3e87561eaf213c2b8f0c5cf6df2d08a
sheydaashoorian <sheydaashoorian@gmail.com>
2026-02-20 21:54:38 +0330

    add user-create dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength} from 'class-validator'
+import {ApiProperty} from '@nestjs/swagger'
+
+
+export class CreateUserDto {
+
+    @IsNotEmpty()
+    @ApiProperty()
+    first_name: string;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    last_name: string;
+
+
+    @ApiProperty({ example: 'sheyda@example.com', description: 'ایمیل منحصر به فرد' })
+    @IsEmail({}, { message: 'فرمت ایمیل صحیح نیست' })
+    email: string;
+
+    @ApiProperty({ example: 'password123', description: 'رمز عبور حداقل ۶ کاراکتر' })
+    @IsString()
+    @MinLength(6, { message: 'رمز عبور باید حداقل ۶  کاراکتر باشد' })
+    password: string;
+ 
+
+}
