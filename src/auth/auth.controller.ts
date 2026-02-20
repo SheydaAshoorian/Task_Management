433e0368296533ee6e30c240fb906ac00955e896
sheydaashoorian <sheydaashoorian@gmail.com>
2026-02-20 21:34:04 +0330

    1. add login dto  2. add login controller in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
+@ApiTags('Auth') // دسته‌بندی در سواگر
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @ApiOperation({ summary: 'Login user and return JWT token' })
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsNotEmpty,password,MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger' ;
+
+
+export class LoginDto {
+
+    @ApiProperty( {example: 'example@gmail.com'} )
+    @IsEmail({} , { message: 'ایمیل وارد شده معتبر نیست'} )
+    email: string ;
+    
+    @ApiProperty( {example: 'password123'} )
+    @IsNotEmpty( { message: ' پسوورد نمیتواند خالی باشد'} )
+    @MinLength(6)
+    @IsString()
+    password: string ;
+
+}
