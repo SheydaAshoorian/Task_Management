840e5d4a23917da8099f213e6477d8967271c256
sheydaashoorian <sheydaashoorian@gmail.com>
2026-02-21 13:45:31 +0330

    add create task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,34 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'; // گارد خودت را ایمپورت کن
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Tasks')
+@ApiBearerAuth() // این برای نمایش آیکون قفل در سواگر است
+@UseGuards(JwtAuthGuard) 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
-  create(@Body() createTaskDto: CreateTaskDto) {
-    return this.tasksService.create(createTaskDto);
+  // @Req (که اطلاعات کاربر لاگین شده هست و از توکن استخراج شده)
+  //@Body (که عنوان و توضیحات تسک هست و کاربر می‌فرسته)
+  create(@Body() createTaskDto: CreateTaskDto, @Req() req) {
+    //  (آیدی منحصر‌به‌فرد کاربر که از توکن گرفته می شود )req.user.sub
+    return this.tasksService.create(createTaskDto, req.user.sub);  
   }
 
   @Get()
-  findAll() {
-    return this.tasksService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tasksService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return this.tasksService.update(+id, updateTaskDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tasksService.remove(+id);
+  findAll(@Req() req) {
+    return this.tasksService.findAll(req.user.sub);
   }
 }
